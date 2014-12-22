ce2cd3e24ef3127d7efd9eaa521957a70e847d2b
Greg <greg.underwood@gmail.com>
2014-12-21 19:42:36 -0500

    Added prefabs for pieces, test placement via keystroke. Realized sprites were too small at 30x30 so started rebuilding them at 100x100 pixels.

---
+++ Assets/CreatePiece_BackL.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePiece_BackL : MonoBehaviour {
+
+	public Transform Piece;
+	public Transform StartLocation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// INITIAL TESTING --
+		// Press space to create piece
+
+		bool createPiece = Input.GetKeyUp("space");
+
+		if (createPiece)
+		{
+			Instantiate(Piece, StartLocation.transform.position, Quaternion.identity);
+		}
+		
+	
+	}
+}
