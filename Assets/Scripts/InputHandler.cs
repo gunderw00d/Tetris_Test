20c8fdb4e488e7d50b95c81ab9a93997b53aa546
Greg <greg.underwood@gmail.com>
2015-02-05 22:37:42 -0500

    Added licensing info to all source files.

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿// Copyright Greg Underwood, 2015.
+// All files in this project, including this one, are covered under the GNU Public License, V3.0.
+// See the file gpl-3.0.txt included in this repository for full details of the license.
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
